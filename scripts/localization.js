74a05a022d1f1c569201306f890abb625348228f
Devin Martin <devin@devin-martin.com>
2015-08-17 22:28:26 -0600

    Add element array

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,8 +1,24 @@
 var localization = {
 	culture: "en-us",
+	elements: [],
 	
+	setCulture(culture) {
+		this.culture = culture;
+		$.each(this.elements, function(index, value) {
+			this.performLocalization(value.element, value.identifier);
+		});
+	},
 	/* replace this function with alternate URL composition schemes */
-	getLocalizedResourceUrl: function(identifier) {
-		return "/resources/" + identifier + "/" + this.culture;
+	getLocalizedResourceUrl: function(identifier, currentCulture) {
+		return "/resources/" + identifier + "/" + currentCulture;
+	},
+	
+	/* takes a dom element and an identifier and performs localization */
+	localize: function(element, identifier) {
+		this.elements.push({element:element, identifier:identifier});
+		this.performLocalization(element, identifier);
+	},
+	
+	performLocalization: function(element, identifier) {
 	}
 };
